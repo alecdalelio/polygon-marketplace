9a7053aa3fa29c99952a76224347f5857ed993ba
alecdalelio <alecdalelio@gmail.com>
2023-06-05 19:45:21 -0400

    create foundation for nft and marketplace contracts, hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
 module.exports = {
-  solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  solidity: "0.8.4",
 };
