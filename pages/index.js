04964ea64fae6dd7c93809fd07169061029a8d3a
alecdalelio <alecdalelio@gmail.com>
2023-06-06 18:07:25 -0400

    build home page functions and deploy configuration

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,122 @@
-import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers"; // Import ethers from the correct package
+
+import { nftAddress, nftMarketAddress } from "../config";
+
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import NFTMarket from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  async function loadNFTs() {
+    const provider = new ethers.providers.JsonRpcProvider(); // Correctly initialize JsonRpcProvider
+    const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(
+      nftMarketAddress,
+      NFTMarket.abi,
+      provider
+    );
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        const price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        const item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+
+  async function buyNft(nft) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      nftMarketAddress,
+      NFTMarket.abi,
+      signer
+    );
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+
+    const transaction = await contract.createMarketSale(
+      nftAddress,
+      nft.tokenId,
+      {
+        value: price,
+      }
+    );
+    await transaction.wait();
+    loadNFTs();
+  }
+
+  if (loadingState === "loaded" && nfts.length === 0) {
+    // Fixed the condition for checking if nfts is empty
+    return (
+      <h1 className="px-20 py-10 text-3xl">No items in the marketplace</h1>
+    );
+  }
+
+  return (
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: "1600px" }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 pt-4">
+          {" "}
+          {/* Fixed the typo in lg_grid-cols-4 */}
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <img src={nft.image} alt="NFT" />{" "}
+              {/* Added alt attribute to the image */}
+              <div className="p-4">
+                <p
+                  style={{ height: "64px" }}
+                  className="text-2xl font-semibold"
+                >
+                  {nft.name}
+                </p>
+                <div style={{ height: "70px", overflow: "hidden" }}>
+                  <p className="text-gray-400">{nft.description}</p>
+                </div>
+              </div>
+              <div className="p-4 bg-black">
+                <p className="text-2xl mb-4 font-bold text-white">
+                  {" "}
+                  {/* Fixed the typo in mb-04 */}
+                  {nft.price} MATIC
+                </p>
+                <button
+                  className="w-full bg-blue-500 text-white font-bold py-2 px-12 rounded"
+                  onClick={() => buyNft(nft)}
+                >
+                  Buy
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
